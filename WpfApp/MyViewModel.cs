1c099b78eee452243b0051636b11ddb44ffb759a
Christian Mazloum <mazloumchristian@gmail.com>
2016-09-22 11:35:45 +0200

    Add WPF app With MVVM Light
    Created MyViewModel & VMLocator

---
+++ WpfApp/MyViewModel.cs
@@ -0,0 +1,52 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp
+{
+    public class MyViewModel : ViewModelBase
+    {
+        public MyViewModel()
+        {
+            MyCommand = new RelayCommand(action, canExecute);
+            this.ValueLabel = "Hello Label";
+        }
+
+        private string _valueLabel;
+
+        public string ValueLabel
+        {
+            get
+            {
+                return _valueLabel;
+            }
+
+            set
+            {
+                if (_valueLabel == value) return;
+                _valueLabel = value;
+                RaisePropertyChanged("ValueLabel");
+            }
+        }
+
+        public ICommand MyCommand
+        {
+            get; set;
+        }
+
+        private void action()
+        {
+            this.ValueLabel = " Clicked";
+        }
+
+        private bool canExecute()
+        {
+            return (this.ValueLabel != " Clicked");
+        }
+    }
+} 
---
+++ WpfApp/VMLocator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp
+{
+    public class VMLocator
+    {
+        static MyViewModel _myViewModel = new MyViewModel();
+
+        public static MyViewModel MyViewModel1
+        {
+            get
+            {
+                return _myViewModel;
+            }
+
+            set
+            {
+                _myViewModel = value;
+            }
+        }
+    }
+}
